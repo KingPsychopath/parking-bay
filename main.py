065e03df18f36cf119c648bf74c31f65001b76f9
KingPsychopath <dev@KingPsychopat.com>
2024-02-24 06:04:44 +0000

    Modules

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
+"""
+    Main Method.
+    Typical Import Structure -> System, Third Party, Local
+
+"""
+
+import pandas as pd
+
+# import utils.test_process_xml as xml_processor
+from utils import test_process_xml as tpx
+
+
 def main() -> None:
-    for x in enumerate([1, 2, 3, 4, 5]):
-        print(x)
-        if x == 5:
-            break
+    """
+    The entry point for the program.
+    """
+    data = tpx.parse_xml_to_list("./data/parking_bay_data.xml")
+    df = pd.DataFrame(data)
+
+    total_bays = len(df)
+    print(df.info())
+
+    print(f"Total parking bays: {total_bays}")
+    tpx.print_rows("./data/parking_bay_data.xml", 0, 5)
+    # average_occupancy = df['parking_bay_length_metres'].mean()
+    # Replace 'occupancy' with your actual column name
 
 
 if __name__ == "__main__":
---
+++ utils/__init__.py
@@ -0,0 +1,9 @@
+"""
+This package provides utility functions for processing XML and Spark data.
+"""
+
+__all__ = ["process_spark", "process_xml", "test_process_xml"]
+
+from . import process_spark
+from . import process_xml
+from . import test_process_xml
